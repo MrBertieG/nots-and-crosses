cba9dd97742a7b7d60e251d6ba00ec672538e56d
Bertie <albertogheorghita90@gmail.com>
2021-10-28 20:21:41 +0000

    Add user quit input function

--- run.py
+++ run.py
@@ -7,9 +7,23 @@ board = [
 ]
 
 def print_board(board):
+    """
+    Prints the slots vertically to the terminal
+    """
     for row in board:
         for slot in row:
             print(f"{slot}", end="")
         print()
 
 print_board(board)
+
+def quit(user_input):
+    if user_input.lower() == "q":
+        return True
+
+"""
+while True:
+    user_input = input("Please enter a position 1 through 9 or enter 'q' to quit:")
+    if quit(user_input) = True:
+        break
+"""
