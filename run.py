c03d9c42cef54432ac710435d8a24191959739ba
Bertie <albertogheorghita90@gmail.com>
2021-10-28 19:55:23 +0000

    Add function that prints the empty board to the terminal

--- run.py
+++ run.py
@@ -6,4 +6,10 @@ board = [
     ["-", "-", "-"]
 ]
 
-print(board)
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(f"{slot}", end="")
+        print()
+
+print_board(board)
