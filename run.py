3a0f6ebfda23ee7440df1444a14895e14fcf3deb
Bertie <albertogheorghita90@gmail.com>
2021-10-30 14:31:28 +0000

    Add function that establishes the range of the user input

--- run.py
+++ run.py
@@ -27,6 +27,7 @@ def check_input(user_input):
     if not isnum(user_input): return False
     user_input = int(user_input)
     # check if it's 1 - 9
+    if not bounds(user_input): return False
     return True
 
 def isnum(user_input):
@@ -39,6 +40,7 @@ def bounds(user_input):
 
 
 while True:
+    print_board(board)
     user_input = input("Please enter a position 1 through 9 or enter 'q' to quit:")
     if quit(user_input): break
     if not check_input(user_input):
