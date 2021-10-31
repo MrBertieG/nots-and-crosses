b4b4ce81a999fc36e85670b1c799cbd482beda34
Bertie <albertogheorghita90@gmail.com>
2021-10-31 13:59:29 +0000

    Fix fuction which prints the X to the board by calling the function correctly in the while statement

--- run.py
+++ run.py
@@ -6,20 +6,19 @@ board = [
     ["-", "-", "-"]
 ]
 
-user = True 
-turns = 0
+USER = True
+TURNS = 0
 
+
+# Prints the board on the terminal
 def print_board(board):
-    """
-    Prints the slots vertically to the terminal
-    """
     for row in board:
         for slot in row:
             print(f"{slot} ", end="")
         print()
 
 
-#This is the Quit function where the user can exit by pressing 'q'
+# This is the Quit function where the user can exit by pressing 'q'
 def quit(user_input):
 
     if user_input.lower() == "q":
@@ -28,38 +27,42 @@ def quit(user_input):
     else:
         return False
 
-#The function will check if the input is a integer number
+
+# The function will check if the input is a integer number
 def check_input(user_input):
 
-    if not isnum(user_input): 
+    if not isnum(user_input):
         return False
 
     user_input = int(user_input)
 
-    if not bounds(user_input): 
+    if not bounds(user_input):
         return False
 
     return True
 
-#Function will establish if the input is a number 
+
+# Function will establish if the input is a number 
 def isnum(user_input):
 
     if not user_input.isnumeric():
         print("Not a valid number")
         return False
-    else: 
+    else:
         return True
 
-#Function establishes if the user input falls within the range of 1 and 9
+
+# Function establishes if the user input falls within the range of 1 and 9
 def bounds(user_input):
 
     if user_input > 9 or user_input < 1:
         print("This number is out of range, please enter a number between 1 and 9")
         return False
-    else: 
+    else:
         return True
 
-#Function checks if the slot on the board has been already taken
+
+# Function checks if the slot on the board has been already taken
 def not_available(coords, board):
     row = coords[0]
     col = coords[1]
@@ -69,21 +72,24 @@ def not_available(coords, board):
     else:
         return False
 
+
 def coordinates(user_input):
     #This assignemt establishes the position on the board. 
     #If the position is between 0 and 2, if divided by 3 it will always be 0
     row = int(user_input / 3)
     #Finding the remainder of / 3
     col = user_input
-    if col > 2: 
+    if col > 2:
         col = int(col % 3)
     return(row, col)
 
 
-def add_to_board(coords, board):
+#Takes the user input and takes the board as the parameter. it adds the coordinates it havs and check if anything is there. 
+def add_to_board(coords, board, active_user):
+
     row = coords[0]
     col = coords[1]
-    board[row][col] = ""
+    board[row][col] = active_user
 
 
 def current_user(user):
@@ -94,9 +100,10 @@ def current_user(user):
 
 
 while True:
+    active_user = current_user(USER)
     print_board(board)
     user_input = input("Please enter a position 1 through 9 or enter 'q' to quit:")
-    if quit(user_input): 
+    if quit(user_input):
         break
     if not check_input(user_input):
         print("Try again")
@@ -107,3 +114,5 @@ while True:
     if not_available(coords, board):
         print("Position is already taken, please try again.")
         continue
+    add_to_board(coords, board, active_user)
+    USER = not USER
