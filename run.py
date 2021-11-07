90f1a35b45470ac96ea890161cc7c68ac7357b62
Bertie <albertogheorghita90@gmail.com>
2021-11-07 11:15:05 +0000

    Redifine winning function

--- run.py
+++ run.py
@@ -4,11 +4,14 @@ import random
 
 board = [' ' for x in range(10)]
 
+
 def insertLetter(letter, pos):
     board[pos] = letter
 
+
 def spaceIsFree(pos):
     return board[pos] == ' '
+
 
 def printBoard(board):
     print(' ')
@@ -24,6 +27,19 @@ def printBoard(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
     print(' ')
+
+
+    
+def isWinner(bo, le):
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or 
+    (bo[4] == le and bo[5] == le and bo[6] == le) or
+    (bo[1] == le and bo[2] == le and bo[3] == le) or
+    (bo[1] == le and bo[4] == le and bo[7] == le) or
+    (bo[2] == le and bo[5] == le and bo[8] == le) or
+    (bo[3] == le and bo[6] == le and bo[9] == le) or
+    (bo[1] == le and bo[5] == le and bo[9] == le) or
+    (bo[3] == le and bo[5] == le and bo[7] == le))
+
 
 
 # This is the Quit function where the user can exit by pressing 'q'
